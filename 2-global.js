ab2e8e350fc8fefb9f099511440a6fffc59a1340
hukma ulil <hukmaulil23@gmail.com>
2024-01-24 11:03:54 +0700

    first commit

---
+++ 2-global.js
@@ -0,0 +1,11 @@
+//Modules
+const peter = 'peter'
+const john = 'john'
+
+
+const sayHi = (name) => {
+   console.log('hello ' + name);
+}
+
+sayHi(peter)
+sayHi(john);
---
+++ 4-names.js
@@ -0,0 +1,4 @@
+const adam = 'adam';
+const layla = 'laylla';
+
+module.exports = {adam, layla}
